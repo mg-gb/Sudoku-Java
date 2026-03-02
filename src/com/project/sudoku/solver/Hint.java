fccf3eae29c623ad383b08cb17e5d8fe5d4a5b99
Raul Abanto <raulanthony.abanto@cit.edu>
2026-03-02 20:08:01 +0800

    feat: add Hint class with row, col, and num properties

--- src/com/project/sudoku/solver/Hint.java
+++ src/com/project/sudoku/solver/Hint.java
@@ -1,4 +1,15 @@
 package com.project.sudoku.solver;
 
 public class Hint {
+    private final int row, col, num;
+
+    public Hint(int row, int col, int num){
+        this.row = row;
+        this.col = col;
+        this.num = num;
+    }
+
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+    public int getNum() { return num; }
 }
