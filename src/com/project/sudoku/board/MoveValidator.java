977bb9c164403dbe0ecfe4d975b075d31baa057a
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-03-10 23:54:58 +0800

    feat: added bounds validation for row, column, and number inputs before checking the move.

--- src/com/project/sudoku/board/MoveValidator.java
+++ src/com/project/sudoku/board/MoveValidator.java
@@ -1,7 +1,18 @@
 package com.project.sudoku.board;
 
 public class MoveValidator {
-    public static boolean isValidMove(int[][] board, int row, int col, int num) {
+
+    public boolean isValidMove(int[][] board, int[][] solvedBoard, int row, int col, int num){
+        if (!isWithinBounds(row, col, num)) return false;
+
+        if (board[row][col] != 0) return false;
+
+        if(!isSafe(board, row, col, num)) return false;
+
+        return solvedBoard[row][col] == num;
+    }
+
+    public boolean isSafe(int[][] board, int row, int col, int num) {
         for(int c = 0; c < SudokuBoard.GRID_SIZE; ++c) {
             if (board[row][c] == num) {
                 return false;
@@ -27,4 +38,32 @@ public class MoveValidator {
 
         return true;
     }
+
+    private boolean solveBoard(int[][] board) {
+        int n = board.length;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= n; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveBoard(board)) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isWithinBounds(int row, int col, int num) {
+        return row >= 0 && row < SudokuBoard.GRID_SIZE
+                && col >= 0 && col < SudokuBoard.GRID_SIZE
+                && num >= 1 && num <= 9;
+    }
+
 }
