435c4fb9a7cbb7748a645692b28e9d7692b58d20
Raul Abanto <raulanthony.abanto@cit.edu>
2026-03-12 01:14:39 +0800

    docs: update menu option description for solved board display

--- src/com/project/sudoku/game/InputHandler.java
+++ src/com/project/sudoku/game/InputHandler.java
@@ -16,7 +16,7 @@ public class InputHandler {
                 System.out.println("Options:");
                 System.out.println("1 - Enter a move");
                 System.out.println("2 - Get a hint");
-                System.out.println("3 - Show solved board and player stats");
+                System.out.println("3 - Show solved board");
                 System.out.println("0 - Exit");
                 System.out.print("Choice: ");
 
